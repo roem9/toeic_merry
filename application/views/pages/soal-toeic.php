63e51132ddf023bd7786b84a7b6b297b457cc36f
roem09 <muhammadrum04@gmail.com>
2021-12-16 16:55:57 +0700

    add onload

--- application/views/pages/soal-toeic.php
+++ application/views/pages/soal-toeic.php
@@ -31,7 +31,7 @@
                             </div>
                         </div>
                         <div class="form-footer">
-                            <button type="button" class="btn btn-primary w-100 btnSignIn">Masuk</button>
+                            <button type="button" class="btn btn-primary w-100 btnSignIn" style="display:none">Masuk</button>
                         </div>
                     <?php endif;?>
                 </div>
@@ -241,6 +241,11 @@
 <?php $this->load->view("_partials/footer")?>
 
 <script>
+
+    $(window).on('load', function (e) {
+        $(".btnSignIn").show()
+    })
+
     $('body').bind('cut copy paste', function(event) {
         event.preventDefault();
     });
